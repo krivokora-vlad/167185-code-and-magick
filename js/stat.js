c9ca204b6a7317434b47248809177e872f8b5bb2
Vlad <programmist.vke@yandex.ru>
2017-08-24 23:15:26 +0300

    Fix #7 Учебный проект: Холст

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,12 @@
-
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillRect(100, 10, 420, 270);
+
+  var widthRect = 420;
+  var heightRect = 270;
+
+  ctx.fillRect(100, 10, widthRect, heightRect);
   ctx.fillStyle = 'white';
 
-  ctx.fillRect(90, 0, 420, 270);
+  ctx.fillRect(90, 0, widthRect, heightRect);
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
 
   ctx.fillText('Ура вы победили!', 120, 40);
@@ -13,35 +16,34 @@ window.renderStatistics = function (ctx, names, times) {
 
   var max = -1;
 
-  for(var i = 0 ; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
     }
   }
 
-  var histogramWidth = 150;
-  var step = histogramWidth / (max - 0);
+  var histogramHeight = 150;
+  var step = histogramHeight / max;
   var barWidth = 50;
   var indent = 40;
   var initialX = 140;
   var initialY = 240;
 
+  function GetRandomAlpha(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(1);
+  }
 
-  for(var i = 0; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     if (names[i] == 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(initialX + (barWidth + indent) * i, initialY - times[i] * step, indent, times[i] * step);
     }
     else {
-      ctx.fillStyle = 'blue';
-      ctx.globalAlpha = 0.1 + i*0.3; //прозрачный голубой
-      ctx.fillRect(initialX + (barWidth + indent) * i, initialY - times[i] * step, indent, times[i] * step);
+      ctx.fillStyle = 'rgba(0, 0, 255,' + GetRandomAlpha(0.1,0.9) + ')';
     }
-
-    ctx.globalAlpha = 1;
+    ctx.fillRect(initialX + (barWidth + indent) * i, initialY - times[i] * step, indent, times[i] * step);
     ctx.fillStyle = 'black';
     ctx.fillText(names[i], initialX + (barWidth + indent) * i, 260);
-    ctx.fillText(times[i].toFixed(0),initialX + (barWidth + indent) * i, 235 - times[i] * step);
-    }
-  };
+    ctx.fillText(times[i].toFixed(0), initialX + (barWidth + indent) * i, 235 - times[i] * step);
+  }
+};
